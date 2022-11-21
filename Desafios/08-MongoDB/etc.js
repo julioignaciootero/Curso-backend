0fa554f7f6dfdaba41bb75d504213f2316552a76
julioignaciootero <julioignaciootero@gmail.com>
2022-11-21 20:06:54 -0300

    CRUD en mongo

---
+++ Desafios/08-MongoDB/etc.js
@@ -0,0 +1,48 @@
+const productos = [
+    {
+        "title": "Escuadra",
+        "price": 120,
+        "thumbnail": "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-48.png",
+    },
+    {
+        "title": "Transportador",
+        "price": 580,
+        "thumbnail": "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-48.png",
+    },
+    {
+        "title": "lapiz",
+        "price": 900,
+        "thumbnail": "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-48.png",
+    },
+    {
+        "title": "cartuchera",
+        "price": 1700,
+        "thumbnail": "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-48.png",
+    },
+    {
+        "title": "lapicera",
+        "price": 2300,
+        "thumbnail": "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-48.png",
+    },
+    {
+        "title": "tijera",
+        "price": 2860,
+        "thumbnail": "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-48.png",
+    },
+    {
+        "title": "goma",
+        "price": 3350,
+        "thumbnail": "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-48.png",
+    },
+    {
+        "title": "resaltador",
+        "price": 4320,
+        "thumbnail": "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-48.png",
+    },
+    {
+        "title": "cuaderno",
+        "price": 4990,
+        "thumbnail": "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-48.png",
+    }
+
+]
