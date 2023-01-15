eb2ebd610a9465415b63495aef4539c687374a1b
julioignaciootero <julioignaciootero@gmail.com>
2023-01-15 18:59:30 -0300

    Prueba aws

--- Desafios/04-API restful/src/routes/index.js
+++ Desafios/04-API restful/src/routes/index.js
@@ -5,6 +5,11 @@ const routerProductos = require('./productos');
 const router = express.Router();
 
 router.use('/productos', routerProductos);
+router.use('/saludo', async (req, res) => {
+    res.status(200).json({
+        msg: "Hola mundo"
+    })
+});
 
 
 module.exports = router;
