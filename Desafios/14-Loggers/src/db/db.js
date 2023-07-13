48db074152dbefc3949272b7e54ca6222a809a5d
Julio Otero <45604192+julioignaciootero@users.noreply.github.com>
2023-07-13 15:44:50 -0300

    Borrado de Loggers por cuestiones de vulnerabilida

--- Desafios/14-Loggers/src/db/db.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose'
-import dotenv from 'dotenv';
-
-export const initDB = () => {
-    
-    return mongoose.connect(process.env.MONGO_URL)
-}
--- Desafios/14-Loggers/src/middlewares/user.js
+++
@@ -1,6 +0,0 @@
-export const isLoggedIn = (req, res, next) => {
-    console.log(req.isAuthenticated())
-    if(!req.isAuthenticated()) {
-        return res.status(401).json({ ok: false , msg: "Sin autorizacion"})
-    }
-}
